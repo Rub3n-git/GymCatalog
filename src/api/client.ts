16ca3b78a487f7501ecfc47badbfceac34a2d12f
Rub3n-git <ruben.urea@alu,ceacfp.es>
2026-05-04 18:18:29 +0200

    feat: deploy frontend and backend

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 // src/api/client.ts
 import type { Exercise } from '../types/Exercise'
 
-const BASE_URL = 'http://localhost:3000/api/v1'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://gymcatalog-api.onrender.com'
 
 // Función genérica para manejar las peticiones
 const handleResponse = async <T>(response: Response): Promise<T> => {
