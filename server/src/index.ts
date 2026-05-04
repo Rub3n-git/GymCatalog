72a162a98313397abbd5c2b8ec916252bc4e4621
Rub3n-git <ruben.urea@alu,ceacfp.es>
2026-05-04 14:07:25 +0200

    feat: testing and deploiment preparation

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 import exerciseRoutes from './routes/exerciseRoutes'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
